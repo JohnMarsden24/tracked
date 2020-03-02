93e1dc3e2edb0705536a46c50be4e23d99005a4c
Ben Sargeant <ben.stroodcp@gmail.com>
2020-03-02 15:44:03 +0000

    recreated db & models

--- db/migrate/20200302152417_create_deliveries.rb
+++ db/migrate/20200302152417_create_deliveries.rb
@@ -1,14 +1,16 @@
 class CreateDeliveries < ActiveRecord::Migration[6.0]
   def change
     create_table :deliveries do |t|
-      t.references :users, null: false, foreign_key: true
-      t.string :courier
       t.string :name
+      t.string :courier
+      t.string :tracking_number
       t.string :status
       t.string :history
       t.date :expected_arrival_date
       t.date :arrival_date
       t.boolean :delivered
+      t.references :user, null: false, foreign_key: true
+
       t.timestamps
     end
   end
