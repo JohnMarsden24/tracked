5f540b0293b68816ff6ab7a8e2dcde94d49cd617
Ben Sargeant <ben.stroodcp@gmail.com>
2020-03-02 16:26:27 +0000

    correct inheritance added and methods

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -1,2 +1,26 @@
-def new
+class DeliveriesController < ApplicationController
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def find_courier
+    # regex stuff
+  end
+
+  def delivery_params
+  end
+
+  #api stuff
+
 end
