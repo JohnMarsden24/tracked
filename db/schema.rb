9e00a8a1e52e6ce9a50ef95af78a37eccfbdb394
Ben Sargeant <ben.stroodcp@gmail.com>
2020-03-02 14:01:04 +0000

    added schema xml file

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_02_120218) do
+ActiveRecord::Schema.define(version: 2020_03_02_132745) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "deliveries", force: :cascade do |t|
+    t.bigint "users_id", null: false
+    t.string "courier"
+    t.string "name"
+    t.string "status"
+    t.string "history"
+    t.date "expected_arrival_date"
+    t.date "arrival_date"
+    t.boolean "delivered"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["users_id"], name: "index_deliveries_on_users_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.bigint "delivery_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["delivery_id"], name: "index_tags_on_delivery_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +49,6 @@ ActiveRecord::Schema.define(version: 2020_03_02_120218) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "deliveries", "users", column: "users_id"
+  add_foreign_key "tags", "deliveries"
 end
