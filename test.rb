f1166878fd601ef72a19a5f68b9f9a3072ac72fa
John Marsden <john.marsden24@gmail.com>
2020-03-05 16:29:26 +0000

    landing page and footer WIP

--- test.rb
+++ test.rb
@@ -1,19 +1,14 @@
-require('aftership')
-AfterShip.api_key = "7beee5c2-ca2b-49c5-a0c8-ee57c0b18434"
+require 'easypost'
 
-number = "390767773272"
+EasyPost.api_key = "EZAKddbf320e2710454a8bcaf92457322c43XQ7RAPvK0Jj1xPtkVMLoFA"
 
-# couriers = AfterShip::V4::Courier.get
-# AfterShip::V4::Courier.get_all
-# couriers = AfterShip::V4::Courier.detect({:tracking_number => number})
+tracker = EasyPost::Tracker.create({
+  tracking_code: "1840908483",
+  carrier: "DPD-UK"
+})
 
-# puts couriers
+# puts tracker.carrier
 
-# AfterShip::V4::Tracking.create('1ZA6F598D992381375', {:emails => ['a@abcd.com', 'asdfasdfs@gmail.com']})
-puts AfterShip::V4::Tracking.get('fedex-uk', number)
-# AfterShip::V4::Tracking.get_all
-# AfterShip::V4::Tracking.update('ups', '1ZA6F598D992381375', {:title => 'Testing'})
-# AfterShip::V4::Tracking.retrack('ups', '1ZA6F598D992381375')
-# AfterShip::V4::Tracking.delete('ups', '1ZA6F598D992381375')
+# carrier_types = EasyPost::CarrierAccount.types
 
-# AfterShip::V4::LastCheckpoint.get('ups', '1ZA6F598D992381375')
+# puts carrier_types
