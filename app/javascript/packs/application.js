86dc7ed5bfeb35c1fdffe072ba6dcfc3a409bb45
Ben Sargeant <ben.stroodcp@gmail.com>
2020-03-06 10:51:07 +0000

    changed trashcan to x icon, removed javascript

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,16 +3,3 @@ require("@rails/activestorage").start()
 require("channels")
 
 import "bootstrap";
-
-const tags = document.querySelectorAll(".tag")
-
-tags.forEach ((tag) => {
-  const tagData = tag.innerHTML
-  tag.addEventListener("mouseover", (event) =>
-  tag.innerHTML+= "<i class='fas fa-trash-alt'></i>"
-)
-  tag.addEventListener("mouseout", (event) =>
-  tag.innerHTML = tagData
-  )
-})
-
