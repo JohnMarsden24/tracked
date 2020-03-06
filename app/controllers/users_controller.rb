7fbf6d07cbffcd83e404ca6655319dae765d8a51
John Marsden <john.marsden24@gmail.com>
2020-03-06 13:01:56 +0000

    delivery display logic done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,13 @@
 class UsersController < ApplicationController
   def show
-    @user = current_user
+    # @active = Delivery.where(:status != "Delivered")
+    @delivered = current_user.deliveries.where(status: "Delivered")
     @delivery = Delivery.new
     @tag = Tag.new
-
     if params[:query].present?
-      @deliveries = @user.deliveries.includes(:history, :tags).search_by_everything(params[:query])
+      @deliveries = current_user.deliveries.includes(:history, :tags).search_by_everything(params[:query])
     else
-      @deliveries = @user.deliveries.includes(:history, :tags).all
+      @deliveries = current_user.deliveries.where.not(status: "Delivered")
     end
   end
-
 end
