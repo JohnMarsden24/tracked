52c362e656db08b7b3c4ee239a081a9d78537ffe
John Marsden <john.marsden24@gmail.com>
2020-03-09 11:56:20 +0000

    added columns

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,15 @@
 class UsersController < ApplicationController
   def show
-    @delivered = current_user.deliveries.where(status: "Delivered")
+    # @delivered = current_user.deliveries.where(status: "Delivered")
     @delivery = Delivery.new
     @tag = Tag.new
+    # @deliveries = current_user.deliveries
     if params[:query].present?
       @deliveries = current_user.deliveries.includes(:history, :tags).search_by_everything(params[:query])
     else
-      @deliveries = current_user.deliveries.where.not(status: "Delivered")
+      @in_transit = current_user.deliveries.where(status: "In Transit")
+      @delayed = current_user.deliveries.where(status: "Delayed")
+      @delivered = current_user.deliveries.where(status: "Delivered")
     end
   end
 
